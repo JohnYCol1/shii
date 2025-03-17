231e25ffbdd7d61ddf41f3db05ace84ad80314e7
JohnYCol1 <125104384+JohnYCol1@users.noreply.github.com>
2025-03-18 00:16:27 +0700

    Update mining.lua

--- mining.lua
+++ mining.lua
@@ -7,12 +7,12 @@ local TeleportService = game:GetService("TeleportService")
 local player = Players.LocalPlayer
 
 -- 🔧 TWEEN SPEED (Adjust movement speed)
-local tweenSpeed = 5 -- Change this value to make movement faster/slower
+local tweenSpeed = 2.7 -- Change this value to make movement faster/slower
 
 -- 🔹 NEW Start & End Position
 local returnPosition = CFrame.new(-532.117, 338.489, 10.078)
 
-local mineTimeout = 10
+local mineTimeout = 8
 local isRunning = true
 local blacklist = {}
 
